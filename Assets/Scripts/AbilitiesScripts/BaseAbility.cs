5b3f5e73e5fbb352927ac459424f8a46e9786a94
pavlkrat <23092004p@gmail.com>
2024-01-15 13:23:40 +0300

    added some ui
    полоску для перезарядки прикрутил

--- Assets/Scripts/AbilitiesScripts/BaseAbility.cs
+++ Assets/Scripts/AbilitiesScripts/BaseAbility.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.ComponentModel.Design;
 using UnityEngine;
 
 public abstract class BaseAbility
--- Assets/Scripts/AbilitiesScripts/UIController.cs
+++ Assets/Scripts/AbilitiesScripts/UIController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,20 +5,21 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     [SerializeField] Button zoneDamageButton;
-    private TextMeshProUGUI zoneDamageButtonText;
+    [SerializeField] Slider zoneDamageSlider;
     private Color readyColor = Color.green; //temporary UI
     private Color loadingColor = Color.red;
     private bool zoneDamageButtonReady = true;
 
     void Start()
     {
-        zoneDamageButtonText = zoneDamageButton.GetComponentInChildren<TextMeshProUGUI>();
-        zoneDamageButtonText.text = "ZoneDMG";
+        zoneDamageButton.GetComponentInChildren<TextMeshProUGUI>().text = "ZoneDMG";
+        zoneDamageSlider.value = 1;
+        zoneDamageButton.image.color = readyColor;
     }
 
     public void setZoneDamageButtonData(float load, bool ready)
     {
-        zoneDamageButtonText.text = load.ToString();    //temporary UI
+        zoneDamageSlider.value = load;
         if (zoneDamageButtonReady ^ ready) //ready-state updated
         {
             zoneDamageButtonReady = ready;
