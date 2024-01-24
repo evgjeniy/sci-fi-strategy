bd51845b017b1378fe7c77fe4d92d13fdd050e80
evgjeniy <dashkevich2016@mail.ru>
2024-01-24 23:53:39 +0300

    add input-system-compatible buttons

---
+++ Assets/Scripts/Input/UI/InputSystemButtonBridge.cs
@@ -0,0 +1,27 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+
+namespace SustainTheStrain.Input.UI
+{
+    public class InputSystemButtonBridge : OnScreenControl
+    {
+        [SerializeField, InputControl(layout = "Button")] private string _controlPath = "<Keyboard>/r";
+        
+        [field: SerializeField] private float Value { get; set; } = 1.0f;
+
+        protected override string controlPathInternal
+        {
+            get => _controlPath;
+            set => _controlPath = value;
+        }
+
+        public async UniTask Click()
+        {
+            SendValueToControl(Value);
+            await UniTask.NextFrame();
+            SendValueToControl(0.0f);
+        }
+    }
+}
