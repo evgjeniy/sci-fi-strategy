38ef9d7864d9c1456d5bae74afbd0343de884257
Danshicu <Promchuk.danik@gmail.com>
2024-02-03 15:53:06 +0300

    ui factory rework

--- Assets/Scripts/EnergySystem/UI/EnergySystemUIFactory.cs
+++ Assets/Scripts/EnergySystem/UI/EnergySystemUIFactory.cs
@@ -4,6 +4,7 @@ using SustainTheStrain.AbilitiesScripts;
 using SustainTheStrain.Input.UI;
 using SustainTheStrain.ResourceSystems;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace SustainTheStrain.EnergySystem.UI
@@ -16,39 +17,49 @@ namespace SustainTheStrain.EnergySystem.UI
         [SerializeField] private Slider _abilityChargeSliderPrefab;
         [SerializeField] private Transform _standartUIParent;
         [SerializeField] private Transform _abilitiesUIParent;
-        [SerializeField] private Image _backgroundSprite;
+        [SerializeField] private Image _standartBackgroundImageSprite;
+        [SerializeField] private Image _abilityBackgroundImage;
         
         
         public KeyValuePair<IEnergySystem, EnergySystemUI> CreateUI(IEnergySystem system)
         {
             var ui = Instantiate(_energySystemUIPrefab, _standartUIParent);
-            var bg = Instantiate(_backgroundSprite, ui.transform);
-            var button = Instantiate(_energySystemControllButton, bg.transform);
+            Image bg;
+            EnergySystemControllButton button = null;
+            
+            switch (system)
+            {
+                case BaseAbility:
+                {
+                    bg = Instantiate(_abilityBackgroundImage, ui.transform);
+                    var buttonTransform = bg.transform;
+                    buttonTransform.localScale *= 1.4f;
+                    var slider = Instantiate(_abilityChargeSliderPrefab, buttonTransform);
+                    slider.value = 0;
+                    button = Instantiate(_energySystemControllButton, slider.transform);
+                    MAbilitiesUIController.AddControlButton(button.GetComponent<InputSystemButtonBridge>(),slider);
+                    ui.transform.parent = _abilitiesUIParent;
+                    break;
+                }
+                case ResourceGenerator generator:
+                {
+                    bg = Instantiate(_standartBackgroundImageSprite, ui.transform);
+                    button = Instantiate(_energySystemControllButton, bg.transform);
+                    var generatorUI = new GeneratorUI(button.transform, generator);
+                    break;
+                }
+                default:
+                    bg = Instantiate(_standartBackgroundImageSprite, ui.transform);
+                    button = Instantiate(_energySystemControllButton, bg.transform);
+                    break;
+            }
+            
             ui.ControllButton = button;
             button.image.sprite = system.ButtonImage;
             ui.MaxBarsCount = system.MaxEnergy;
             button.OnLeftMouseClick += system.TrySpendEnergy;
             button.OnRightMouseClick += system.TryRefillEnergy;
             system.OnCurrentEnergyChanged += ui.ChangeEnergy;
-            switch (system)
-            {
-                case BaseAbility:
-                {   
-                    var s = Instantiate(_abilityChargeSliderPrefab, button.transform);
-                    s.value = 0;
-                    bg.transform.localScale *= 1.4f;
-                    MAbilitiesUIController.AddControlButton(button.GetComponent<InputSystemButtonBridge>(),s);
-                    ui.transform.parent = _abilitiesUIParent;
-                    break;
-                }
-                case ResourceGenerator generator:
-                {
-                    var generatorUI = new GeneratorUI(button.transform, generator);
-                    break;
-                }
-                default:
-                    break;
-            }
 
             return new KeyValuePair<IEnergySystem, EnergySystemUI>(system, ui);
 
