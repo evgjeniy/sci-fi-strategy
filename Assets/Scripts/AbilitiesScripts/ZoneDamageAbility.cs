3eb135e3ebfc7682506704f966732a1bc073acbd
pavlkrat <23092004p@gmail.com>
2024-02-04 12:09:54 +0300

    added explosions
    propal UI?

--- Assets/Scripts/AbilitiesScripts/ZoneDamageAbility.cs
+++ Assets/Scripts/AbilitiesScripts/ZoneDamageAbility.cs
@@ -5,11 +5,13 @@ namespace SustainTheStrain.AbilitiesScripts
     public class ZoneDamageAbility : ZoneAbility
     {
         protected float damage;
+        protected GameObject ExplosionPrefab;
         public ZoneDamageAbility(ZoneDamageAbilitySettings settings)
         {
             zoneRadius = settings.ZoneRadius;
             LoadingSpeed = settings.ReloadingSpeed;
             damage = settings.Damage;
+            ExplosionPrefab = settings.ExplosionPrefab;
             SetEnergySettings(settings.EnergySettings);
         }
 
@@ -28,6 +30,17 @@ namespace SustainTheStrain.AbilitiesScripts
                 dmg.Damage(damage);
                 //Debug.Log(dmg.CurrentHP);
             }
+            var Explosion = GameObject.Instantiate(ExplosionPrefab, hit.transform, false);
+            Explosion.SetActive(false);
+            var ps = Explosion.GetComponent<ParticleSystem>();
+            var main = ps.main;
+            var sizeCurve = new ParticleSystem.MinMaxCurve();
+            sizeCurve.mode = ParticleSystemCurveMode.TwoConstants;
+            sizeCurve.constantMin = Mathf.Max(0, zoneRadius - 0.5f);
+            sizeCurve.constantMax = zoneRadius + 0.5f;
+            main.startSize = sizeCurve;
+            Explosion.SetActive(true);
+            GameObject.Destroy(Explosion, 1);
         }
 
         protected override void ReadyToShoot()
