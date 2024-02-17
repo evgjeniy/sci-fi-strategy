e4697bc9569302cc4c39b4d80203e2789ac85e37
evgjeniy <dashkevich2016@mail.ru>
2024-02-17 15:38:38 +0300

    base setup folders structure

---
+++ Assets/Code/Editor/Units/RoadSignEditor.cs
@@ -0,0 +1,34 @@
+using Dreamteck.Splines;
+using UnityEditor;
+using UnityEngine;
+
+namespace SustainTheStrain.Units.Editor
+{
+
+    [CustomEditor(typeof(RoadSign))]
+    public class RoadSignEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            RoadSign roadSign = (RoadSign)target;
+            var node = roadSign.GetComponent<Node>();
+
+            DrawDefaultInspector();
+            
+            GUILayout.Space(10);
+
+            if (roadSign.Guides == null || roadSign.Guides.Length != node.GetConnections().Length)
+                roadSign.Guides = new bool[node.GetConnections().Length];
+            
+            for (int i = 0; i < node.GetConnections().Length; i++)
+            {
+                EditorGUILayout.BeginHorizontal("box");
+                
+                roadSign.Guides[i] = EditorGUILayout.Toggle(roadSign.Guides[i]);
+                EditorGUILayout.LabelField($"{node.GetConnections()[i].spline.name} at point {node.GetConnections()[i].pointIndex}");
+
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+    }
+}
