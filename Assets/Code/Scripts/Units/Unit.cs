3204ccc6c469140128dcebb6597f68414de85f37
Shtrudell <ilya.naumenko.work@gmail.com>
2024-04-01 18:30:40 +0300

    level setup

--- Assets/Code/Scripts/Units/Unit.cs
+++ Assets/Code/Scripts/Units/Unit.cs
@@ -43,7 +43,7 @@ namespace SustainTheStrain.Units
             AggroRadiusCheck.OnUnitLeftAggroZone += UnitLeftAggroZone;
 
             AttackRadiusCheck = GetComponentInChildren<AttackRadiusCheck>();
-            AttackRadiusCheck.OnUnitEnteredAttackZone += UnitEneterdAttackZone;
+            AttackRadiusCheck.OnUnitEnteredAttackZone += UnitEnteredAttackZone;
             AttackRadiusCheck.OnUnitLeftAttackZone += UnitLeftAttackZone;
 
             NavPathFollower = new NavPathFollower(GetComponent<NavMeshAgent>());
@@ -65,12 +65,7 @@ namespace SustainTheStrain.Units
             IsOpponentInAggroZone = Duelable.Opponent == unit;
         }
 
-        private void UnitEneterdAttackZone(Duelable unit)
-        {
-            IsOpponentInAttackZone = unit == Duelable.Opponent;
-        }
-
-        private void UnitLeftAttackZone(Duelable unit)
+        private void UnitEnteredAttackZone(Duelable unit)
         {
             if (!Duelable.HasOpponent)
             {
@@ -78,7 +73,21 @@ namespace SustainTheStrain.Units
                 return;
             }
             
-            IsOpponentInAttackZone = !(unit == Duelable.Opponent);
+            if(IsOpponentInAttackZone) return;
+            
+            IsOpponentInAttackZone = unit == Duelable.Opponent;
+        }
+
+        private void UnitLeftAttackZone(Duelable unit)
+        {
+            if (!Duelable.HasOpponent) 
+            {
+                IsOpponentInAttackZone = false;
+                return;
+            }
+            
+            if(IsOpponentInAttackZone)
+                IsOpponentInAttackZone = !(unit == Duelable.Opponent);
         }
 
         #endregion
---
+++ Assets/Plugins/ARTnGAME/EXTRAS/kMotion/Runtime/MotionBlurRenderPass.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace kTools.Motion
+{
+    sealed class MotionBlurRenderPass : ScriptableRenderPass
+    {
+#region Fields
+        const string kMotionBlurShader = "Hidden/kMotion/MotionBlur";
+        const string kProfilingTag = "Motion Blur";
+
+        static readonly string[] s_ShaderTags = new string[]
+        {
+            "UniversalForward",
+            "LightweightForward",
+        };
+
+        Material m_Material;
+        MotionBlur m_MotionBlur;
+#endregion
+
+#region Constructors
+        internal MotionBlurRenderPass()
+        {
+            // Set data
+            //renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;//v1.2.0
+            renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;//v1.2.0
+        }
+#endregion
+
+#region Setup
+        internal void Setup(MotionBlur motionBlur)
+        {
+            // Set data
+            m_MotionBlur = motionBlur;
+            m_Material = new Material(Shader.Find(kMotionBlurShader));
+        }
+#endregion
+
+#region Execution
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            // Get data
+            var camera = renderingData.cameraData.camera;
+
+            // Never draw in Preview
+            if(camera.cameraType == CameraType.Preview)
+                return;
+
+            // Profiling command
+            CommandBuffer cmd = CommandBufferPool.Get(kProfilingTag);
+            //using (new ProfilingSample(cmd, kProfilingTag)) //v1.2.0
+            {
+                // Set Material properties from VolumeComponent
+                m_Material.SetFloat("_Intensity", m_MotionBlur.intensity.value);
+
+                // TODO: Why doesnt RenderTargetHandle.CameraTarget work?
+                //var colorTextureIdentifier = new RenderTargetIdentifier("_CameraColorTexture");
+                var colorTextureIdentifier = renderingData.cameraData.renderer.cameraColorTarget; //v1.2.0
+
+                // RenderTexture
+                var descriptor = new RenderTextureDescriptor(camera.scaledPixelWidth, camera.scaledPixelHeight, RenderTextureFormat.DefaultHDR, 16);
+                var renderTexture = RenderTexture.GetTemporary(descriptor);
+
+                // Blits
+                var passIndex = (int)m_MotionBlur.quality.value;
+                cmd.Blit(colorTextureIdentifier, renderTexture, m_Material, passIndex);
+                cmd.Blit(renderTexture, colorTextureIdentifier, m_Material, passIndex);
+                ExecuteCommand(context, cmd);
+
+                RenderTexture.ReleaseTemporary(renderTexture);
+            }
+            ExecuteCommand(context, cmd);
+        }
+#endregion
+
+#region CommandBufer
+        void ExecuteCommand(ScriptableRenderContext context, CommandBuffer cmd)
+        {
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+        }
+#endregion
+    }
+}
