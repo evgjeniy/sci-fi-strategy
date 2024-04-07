fc20ff7f14129131702d137a4d6fd7a2aac96940
evgjeniy <dashkevich2016@mail.ru>
2024-04-07 16:11:52 +0300

    add PointerEnter and PointerExit in `selection` state

--- Assets/Code/Scripts/Input/NewInput/States/SelectableState.cs
+++ Assets/Code/Scripts/Input/NewInput/States/SelectableState.cs
@@ -1,8 +1,12 @@
-﻿namespace SustainTheStrain.Input
+﻿using UnityEngine;
+
+namespace SustainTheStrain.Input
 {
     public class SelectableState : IInputState
     {
         private readonly IInputSelectable _selectable;
+
+        private IInputSelectable _pointerSelectable;
 
         public SelectableState(IInputSelectable selectable) => _selectable = selectable;
 
@@ -10,25 +14,55 @@
 
         public void Exit() => _selectable.OnDeselected();
 
-        public IInputState ProcessMouseMove(IInputSystem context) =>
-            context.MousePosition.TryGetCameraRay(out var screenRay)
-                ? _selectable.OnSelectedUpdate(this, screenRay)
-                : this;
+        public IInputState ProcessMouseMove(IInputSystem context)
+        {
+            if (context.MousePosition.TryGetCameraRay(out var screenRay) is false)
+                return this;
+
+            UpdatePointerSelectable(context.Settings, screenRay);
+
+            return _selectable.OnSelectedUpdate(this, screenRay);
+        }
 
         public IInputState ProcessLeftClick(IInputSystem context)
         {
             if (context.MousePosition.TryGetCameraRay(out var screenRay) is false)
                 return this;
 
-            if (screenRay.TryGetRaycastComponent<IInputSelectable>(context.Settings, out var selectable))
-                return _selectable == selectable ? this : new SelectableState(selectable);
+            if (_pointerSelectable != null)
+            {
+                _pointerSelectable.OnPointerExit();
+                return new SelectableState(_pointerSelectable);
+            }
 
             return _selectable.OnSelectedLeftClick(this, screenRay);
         }
 
-        public IInputState ProcessRightClick(IInputSystem context) =>
-            context.MousePosition.TryGetCameraRay(out var screenRay)
-                ? _selectable.OnSelectedRightClick(this, screenRay)
-                : this;
+        public IInputState ProcessRightClick(IInputSystem context)
+        {
+            var hasRay = context.MousePosition.TryGetCameraRay(out var screenRay);
+            return hasRay ? _selectable.OnSelectedRightClick(this, screenRay) : this;
+        }
+
+        private void UpdatePointerSelectable(InputSettings settings, Ray screenRay)
+        {
+            var hasSelectable = screenRay.TryGetRaycastComponent<IInputSelectable>(settings, out var pointerSelectable);
+            if (pointerSelectable == _selectable) return;
+            
+            if (hasSelectable)
+            {
+                if (_pointerSelectable != null) return;
+                
+                _pointerSelectable = pointerSelectable;
+                _pointerSelectable.OnPointerEnter();
+            }
+            else
+            {
+                if (_pointerSelectable == null) return;
+                
+                _pointerSelectable.OnPointerExit();
+                _pointerSelectable = null;
+            }
+        }
     }
 }
