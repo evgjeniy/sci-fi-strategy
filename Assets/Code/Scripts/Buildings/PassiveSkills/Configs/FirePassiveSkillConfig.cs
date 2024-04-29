70f6603e798e9fc1e32a327ff1c99c6b2b6a1259
evgjeniy <dashkevich2016@mail.ru>
2024-04-29 18:19:59 +0300

    remove base class

--- Assets/Code/Scripts/Buildings/PassiveSkills/Configs/FirePassiveSkillConfig.cs
+++ Assets/Code/Scripts/Buildings/PassiveSkills/Configs/FirePassiveSkillConfig.cs
@@ -1,27 +1,20 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace SustainTheStrain.Buildings
 {
     [CreateAssetMenu(fileName = nameof(FirePassiveSkillConfig), menuName = "Configs/" + nameof(FirePassiveSkillConfig), order = Const.Order.BuildingConfigs)]
-    public class FirePassiveSkillConfig : PassiveSkillConfig
+    public class FirePassiveSkillConfig : ScriptableObject
     {
         [field: SerializeField, Min(0.0f)] public float DamagePerSecond { get; private set; }
         [field: SerializeField, Min(0.0f)] public float FireDuration { get; private set; }
         [field: SerializeField, Min(0)] public int AttackFrequency { get; private set; }
         
-        public override void EnableSkill(GameObject gameObject)
+        public void EnableSkill(GameObject gameObject)
         {
             var effect = gameObject.GetComponent<FireEffect>();
             if (effect == null) effect = gameObject.AddComponent<FireEffect>();
             
             effect.Initialize(DamagePerSecond, FireDuration);
-        }
-
-        public override void DisableSkill(GameObject gameObject)
-        {
-            if (gameObject.TryGetComponent<FireEffect>(out var effect)) 
-                Destroy(effect);
         }
     }
 }
--- Assets/Code/Scripts/Buildings/PassiveSkills/Configs/ShieldDeactivatorConfig.cs
+++ Assets/Code/Scripts/Buildings/PassiveSkills/Configs/ShieldDeactivatorConfig.cs
@@ -1,26 +1,19 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace SustainTheStrain.Buildings
 {
     [CreateAssetMenu(fileName = nameof(ShieldDeactivatorConfig), menuName = "Configs/" + nameof(ShieldDeactivatorConfig), order = Const.Order.BuildingConfigs)]
-    public class ShieldDeactivatorConfig : PassiveSkillConfig
+    public class ShieldDeactivatorConfig : ScriptableObject
     {
         [field: SerializeField, Min(0.0f)] public float Duration { get; private set; } = 1.5f;
         [field: SerializeField, Min(0)] public int AttackFrequency { get; private set; } = 2;
         
-        public override void EnableSkill(GameObject gameObject)
+        public void EnableSkill(GameObject gameObject)
         {
             var effect = gameObject.GetComponent<ShieldDeactivatorEffect>();
             if (effect == null) effect = gameObject.AddComponent<ShieldDeactivatorEffect>();
             
             effect.Initialize(Duration);
-        }
-
-        public override void DisableSkill(GameObject gameObject)
-        {
-            if (gameObject.TryGetComponent<ShieldDeactivatorEffect>(out var effect)) 
-                Destroy(effect);
         }
     }
 }
