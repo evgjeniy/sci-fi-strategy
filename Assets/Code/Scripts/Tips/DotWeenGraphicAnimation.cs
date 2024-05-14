4a8142bde1c74305074ec52fbb02710220d84f66
evgjeniy <dashkevich2016@mail.ru>
2024-05-14 21:29:13 +0300

    fix UI, add new font

--- Assets/Code/Scripts/Tips/DotWeenGraphicAnimation.cs
+++ Assets/Code/Scripts/Tips/DotWeenGraphicAnimation.cs
@@ -2,6 +2,7 @@
 using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace SustainTheStrain.Tips
@@ -10,12 +11,14 @@ namespace SustainTheStrain.Tips
     {
         [Foldout("Show Settings"), SerializeField, Min(0.0f)] private float _showDelay = 0.5f;
         [Foldout("Show Settings"), SerializeField, Min(0.0f)] private float _showDuration = 0.5f;
+        [Foldout("Show Settings"), SerializeField, Range(0.0f, 1.0f)] private float _showFadeValue = 1.0f;
         [Foldout("Show Settings"), SerializeField] private Ease _showEase = Ease.OutExpo;
         [Foldout("Show Settings"), SerializeField] private UnityEvent _onShowStart;
         [Foldout("Show Settings"), SerializeField] private UnityEvent _onShowComplete;
 
         [Foldout("Hide Settings"), SerializeField, Min(0.0f)] private float _hideDelay = 0.0f;
         [Foldout("Hide Settings"), SerializeField, Min(0.0f)] private float _hideDuration = 0.0f;
+        [Foldout("Show Settings"), SerializeField, Range(0.0f, 1.0f)] private float _hideFadeValue = 0.0f;
         [Foldout("Hide Settings"), SerializeField] private Ease _hideEase = Ease.Unset;
         [Foldout("Hide Settings"), SerializeField] private UnityEvent _onHideStart;
         [Foldout("Hide Settings"), SerializeField] private UnityEvent _onHideComplete;
@@ -25,7 +28,7 @@ namespace SustainTheStrain.Tips
         public void Show(Graphic graphic)
         {
             _tween?.Kill();
-            _tween = graphic.DOFade(1.0f, _showDuration)
+            _tween = graphic.DOFade(_showFadeValue, _showDuration)
                 .SetDelay(_showDelay).SetEase(_showEase)
                 .SetLink(graphic.gameObject)
                 .OnStart(_onShowStart.Invoke)
@@ -36,7 +39,7 @@ namespace SustainTheStrain.Tips
         public void Hide(Graphic graphic)
         {
             _tween?.Kill();
-            _tween = graphic.DOFade(0.0f, _hideDuration)
+            _tween = graphic.DOFade(_hideFadeValue, _hideDuration)
                 .SetDelay(_hideDelay).SetEase(_hideEase)
                 .SetLink(graphic.gameObject)
                 .OnStart(_onHideStart.Invoke)
