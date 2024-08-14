ab21c0fd22320dd53a740b690f589c93d83f814a
Buttonchick <112191305+Buttonchick@users.noreply.github.com>
2024-08-14 15:34:53 +0300

    Test Ui Buttons functions added

--- Assets/TestUI.cs
+++ Assets/TestUI.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using SustainTheStrain.EnergySystem;
 using SustainTheStrain.ResourceSystems;
 using UnityEngine;
+using VolumetricFogAndMist;
 using Zenject;
 
 public class TestUI : MonoBehaviour
 {
-    [Inject] private ResourceManager manager;
+    [Inject] private ResourceManager resourceManager;
     [Inject] private EnergyController energyManager;
     
     
@@ -15,13 +16,35 @@ public class TestUI : MonoBehaviour
     {
         
     }
+
+    public void SetGold(int amount){
+
+        int minAmount = 0;
+        int maxAmount = 999999;
+
+        int clampedAmount= Mathf.Clamp(amount, minAmount, maxAmount);
+
+        int currentGold = resourceManager.Gold;
+        int diff = clampedAmount - currentGold;
+
+        resourceManager.AddGold(diff);
+    }
+
+    public void AddGold(int amount){
+
+        int currentGold = resourceManager.Gold;
+        int sum = currentGold + amount;
+
+        SetGold(sum);
+            
+    }
     
     
     public void ZeroEnergy()
     {
         foreach (var system in energyManager.Systems)
         {
-            for (int i=0; i<= system.CurrentEnergy; i++)
+            for (int i=0; i<= system.CurrentEnergy + 1; i++)
             {
                 energyManager.TryReturnEnergyFromSystem(system);
             }
